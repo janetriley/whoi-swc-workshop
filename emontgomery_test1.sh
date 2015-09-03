691c34c80a34272c3750869a7ce8037da1907fdf
emontgomery-usgs <emontgomery@usgs.gov>
2015-09-03 15:12:32 -0400

    added another interesting detail

--- emontgomery_test1.sh
+++ emontgomery_test1.sh
@@ -1,6 +1,8 @@
 # script to test command line inputs
 #
 # usage  test1.sh *.dat 5 1
+Ellyn had a funny spelling of her name
+# this is an example from class.
 echo printing 1st line of all the files
 head -$2 $1
 echo printing last line of all the files
